b59f9ccbd2251ee137a1b873ec66c252c0eb0354
bcostantino <costan96@students.rowan.edu>
2023-05-08 12:22:55 -0400

    added structure for servo control

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -19,6 +19,14 @@
 #define PWM_PERIOD (20000)
 #define NUM_SERVOS 3
 
+
+int16_t random_int16(int16_t A, int16_t B) {
+    uint32_t randval = esp_random();
+    int32_t range = (int32_t)B - (int32_t)A + 1;
+    int32_t scaled = (int32_t)randval * range / UINT32_MAX;
+    return (int16_t)(scaled + A);
+}
+
 const uint32_t servo_pins[NUM_SERVOS] = {
 	GPIO_NUM_14,
 	GPIO_NUM_4,
@@ -26,29 +34,29 @@ const uint32_t servo_pins[NUM_SERVOS] = {
 };
 
 void initialize_gpio() {	
-    gpio_config_t io_conf;
-    io_conf.intr_type = GPIO_INTR_POSEDGE;
-    io_conf.mode = GPIO_MODE_INPUT;
-    io_conf.pin_bit_mask = GPIO_Pin_13;
-    //io_conf.pull_down_en = 1;
+	gpio_config_t io_conf;
+	io_conf.intr_type = GPIO_INTR_POSEDGE;
+	io_conf.mode = GPIO_MODE_INPUT;
+	io_conf.pin_bit_mask = GPIO_Pin_13;
+	//io_conf.pull_down_en = 1;
 
-    gpio_config(&io_conf);
+	gpio_config(&io_conf);
 
 }
 
 
 void initialize_servos(servo_control_t *servos) {
-	
+
 	for(int i = 0; i < NUM_SERVOS; ++i) {
 		servos[i].pwm_period = PWM_PERIOD;
 		servos[i].min_pulse_width = 500;
 		servos[i].max_pulse_width = 2500;
 		servos[i].angle_range = 180;
 	}
+
 }
 
 void reset_angle(servo_control_t *servos, uint8_t channel_num, int16_t angle) {	
-	//pwm_stop(0x0);
 	uint32_t duty = angle_pulsewidth_ii(&(servos[channel_num]), angle);
 	printf("Setting servo on channel %u to angle %d deg, mapped to PWM duty %u us\n", channel_num, angle, duty);
 	pwm_set_duty(channel_num, duty);
@@ -66,74 +74,80 @@ void map_angles(uint32_t *duties, servo_control_t *servos, int num_servos, int16
 
 const int16_t NEUTRAL_POSITION[3] = { 0, 0, 0 };
 
-const int routine_a_len = 6;
-const int16_t _routine_a[6][3] = {
-	{ 0, 0, 0 },
-	{ 0, -90, -90 },
-	{ 0, 90, 90 },
-	{ -90, 0, 0 },
-	{ -90, 90, 90 },
-	{ -90, -90, -90 }
-};
-
-void routine_a(servo_control_t *servos) {
-	for(int i = 0; i < routine_a_len; ++i) {
-		
-		int16_t *angles = _routine_a[i];
-		uint32_t duties[NUM_SERVOS];
-
-		map_angles(duties, servos, NUM_SERVOS, angles);
-		pwm_set_duties(duties);
-		pwm_start();
-
-		vTaskDelay(1000 / portTICK_RATE_MS);
-	}
-} 
-
-void move_to_neutral(servo_control_t *servos, int num_servos) {
+/**
+ * 
+ */
+void move_servos(servo_control_t *servos, int16_t *control_vector, int num_servos) {	
 	uint32_t duties[num_servos];
-	map_angles(duties, servos, NUM_SERVOS, NEUTRAL_POSITION);
+	map_angles(duties, servos, NUM_SERVOS, control_vector);
 	pwm_set_duties(duties);
 	pwm_start();
 }
 
-/*struct gpio_isr_args {
-	servo_control_t *servos;
-	int num_servos;
-	int *counter;
+void move_to_neutral(servo_control_t *servos, int num_servos) {
+	// uint32_t duties[num_servos];
+	// map_angles(duties, servos, NUM_SERVOS, NEUTRAL_POSITION);
+	// pwm_set_duties(duties);
+	// pwm_start();
+
+	move_servos(servos, NEUTRAL_POSITION, num_servos);
+
+}
+
+const int routine_a_len = 7;
+const int16_t _routine_a[7][3] = {
+	{ 0, 0, 0 },
+	{ 0, -90, 90 },
+	{ 0, 90, -90 },
+	{ -90, 0, 0 },
+	{ -90, 90, -90 },
+	{ -90, -90, 90 },
+	{ 0, 0, 0 }
 };
 
-int counter = 0;
-void gpio_isr_handler(void *arg) {
-	struct gpio_isr_args *_args = (struct gpio_isr_args *)arg;
-	//move_to_neutral(_args->servos, _args->num_servos);
-}*/
+void routine_a(servo_control_t *servos) {
+	for(int i = 0; i < routine_a_len; ++i) {
 
-static xQueueHandle gpio_evt_queue = NULL;
+		int16_t *angles = _routine_a[i];
+		move_servos(servos, angles, NUM_SERVOS);
 
+		vTaskDelay(1000 / portTICK_RATE_MS);
+	}
+}
+
+
+typedef struct {
+	uint32_t gpio_num;			/* gpio pin number that triggered control task */
+	uint8_t servo_count;		/* number of servos */
+	int16_t *position_vector;	/* array of len servo_count, contains angles */
+} servo_control_args_t;
+
+
+static xQueueHandle servo_control_queue = NULL;
 static void reset_servos_isr_handler(void *arg) {
-	uint32_t gpio_num = (uint32_t)arg;
-    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
+	//uint32_t gpio_num = (uint32_t)arg;
+	servo_control_args_t *args = (servo_control_args_t*)arg;
+	xQueueSendFromISR(servo_control_queue, args, NULL);
 }
 
 static void servo_control_task(void *arg) {
 
 	printf("Initializing servos and PWM channels\n");
-    servo_control_t *servos;	
+	servo_control_t *servos;
 	servos = (servo_control_t*)heap_caps_calloc(NUM_SERVOS, sizeof(servo_control_t), MALLOC_CAP_32BIT);
 	initialize_servos(servos);
 
 	uint32_t init_duty[NUM_SERVOS];
 	float phases[NUM_SERVOS];
-	 
+
 	for(int i = 0; i < NUM_SERVOS; ++i) {
-	 	init_duty[i] = angle_pulsewidth_ii(&(servos[i]), 0);
-	 	phases[i] = 0;
-	 	printf("[SRVO]: servo channel #%d:\n", i);
-	 	printf("\tconfig: period = %u us, min_pw = %u us, max_px = %u us, range = %d deg\n", 
-	 			servos[i].pwm_period, servos[i].min_pulse_width,
-	 			servos[i].max_pulse_width, servos[i].angle_range);
-	 	printf("\tinitial: pw = %u us, phase = %f\n", init_duty[i], phases[i]);
+		init_duty[i] = angle_pulsewidth_ii(&(servos[i]), 0);
+		phases[i] = 0;
+		printf("[SRVO]: servo channel #%d:\n", i);
+		printf("\tconfig: period = %u us, min_pw = %u us, max_px = %u us, range = %d deg\n", 
+				servos[i].pwm_period, servos[i].min_pulse_width,
+				servos[i].max_pulse_width, servos[i].angle_range);
+		printf("\tinitial: pw = %u us, phase = %f\n", init_duty[i], phases[i]);
 	}
 
 	pwm_init(PWM_PERIOD, init_duty, NUM_SERVOS, servo_pins);
@@ -141,82 +155,69 @@ static void servo_control_task(void *arg) {
 
 	printf("Moving to neutral position...\n");
 	move_to_neutral(servos, NUM_SERVOS);
-    
 
+	int16_t last_positon[NUM_SERVOS];
 	for (;;) {
-		uint32_t io_num;
-		
-		/* see https://www.freertos.org/a00118.html */
-        if (xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
-            //ESP_LOGI(TAG, "GPIO[%d] intr, val: %d\n", io_num, gpio_get_level(io_num));
-			printf("Resetting servo positions...\n");
-			routine_a(servos);
-        }
-    }
+		//uint32_t io_num;
+		servo_control_args_t args;
+
+		/* execute block if queue item exists
+		 * see https://www.freertos.org/a00118.html */
+		if (xQueueReceive(servo_control_queue, &args, portMAX_DELAY)) {
+			//ESP_LOGI(TAG, "GPIO[%d] intr, val: %d\n", io_num, gpio_get_level(io_num));
+			printf("Triggered by GPIO:%d, setting %d servo positions to ", args.gpio_num, args.servo_count);
+			printf("[ %d, %d, %d ]\n",
+					args.position_vector[0],
+					args.position_vector[1],
+					args.position_vector[2]);
+			//move_to_neutral(servos, NUM_SERVOS);
+			//routine_a(servos);
+		}
+	}
 
 	heap_caps_free(servos);
 
 }
 
 
+
 void app_main() {
-    /* Print chip information */
-    esp_chip_info_t chip_info;
-    esp_chip_info(&chip_info);
-    printf("This is ESP8266 chip with %d CPU cores, WiFi, ",
-            chip_info.cores);
-
-    printf("silicon revision %d, ", chip_info.revision);
-
-    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
-            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
-
-	// printf("Initializing servos and PWM channels\n");
-    // servo_control_t *servos;	
-	// servos = (servo_control_t*)heap_caps_calloc(NUM_SERVOS, sizeof(servo_control_t), MALLOC_CAP_32BIT);
-	// initialize_servos(servos);
-
-	// uint32_t init_duty[NUM_SERVOS];
-	// float phases[NUM_SERVOS];
-	// 
-	// for(int i = 0; i < NUM_SERVOS; ++i) {
-	// 	init_duty[i] = angle_pulsewidth_ii(&(servos[i]), 0);
-	// 	phases[i] = 0;
-
-	// 	printf("[SRVO]: servo channel #%d:\n", i);
-	// 	printf("\tconfig: period = %u us, min_pw = %u us, max_px = %u us, range = %d deg\n", 
-	// 			servos[i].pwm_period, servos[i].min_pulse_width,
-	// 			servos[i].max_pulse_width, servos[i].angle_range);
-	// 	printf("\tinitial: pw = %u us, phase = %f\n", init_duty[i], phases[i]);
-	// }
-
-	// pwm_init(PWM_PERIOD, init_duty, NUM_SERVOS, servo_pins);
-	// pwm_set_phases(phases);
-
-	// printf("Moving to neutral position...\n");
-	// move_to_neutral(servos, NUM_SERVOS);
+	/* Print chip information */
+	esp_chip_info_t chip_info;
+	esp_chip_info(&chip_info);
+	printf("This is ESP8266 chip with %d CPU cores, WiFi, ", chip_info.cores);
+	printf("silicon revision %d, ", chip_info.revision);
+	printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
+			(chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
 
 	printf("Initializing GPIO\n");
 	initialize_gpio();
-    
- 	/* call xQueueCreate(uxQueueLength, uxItemSize) to create queue (kinda event buffer) 
-	 * see http://web.ist.utl.pt/~ist11993/FRTOS-API/group___queue_management.html */
-    gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
- 
+
+	/* call xQueueCreate(uxQueueLength, uxItemSize) to create queue (kinda event buffer) 
+	 * see http://web.ist.utl.pt/~ist11993/FRTOS-API/group___queue_management.html
+	 *	   https://www.freertos.org/Embedded-RTOS-Queues.html
+	 *	   https://www.freertos.org/a00116.html */
+	servo_control_queue = xQueueCreate(10, sizeof(servo_control_args_t));
+
 	/* create task 
 	 * see https://www.freertos.org/a00125.html and https://www.freertos.org/taskandcr.html */
 	xTaskCreate(servo_control_task, "servo_control_task", 2048, NULL, 10, NULL);
-	
+
 	/* add isr handler for gpio */
 	gpio_install_isr_service(0);
-	gpio_isr_handler_add(GPIO_NUM_13, reset_servos_isr_handler, (void*)GPIO_NUM_13);
 
+	//servo_control_args_t *args = { GPIO_NUM_13 };
+	/* allocate args structure + control vector length 
+	 * initialize control vector to 0 */
+	servo_control_args_t *args;
+	args = (servo_control_args_t*)heap_caps_calloc(1, sizeof(servo_control_args_t), MALLOC_CAP_32BIT);
+	args->gpio_num = GPIO_NUM_13;
+	args->servo_count = NUM_SERVOS;
+	args->position_vector = (int16_t*)calloc(args->servo_count, sizeof(int16_t));
+	args->position_vector[0] = 28;
+	gpio_isr_handler_add(GPIO_NUM_13, reset_servos_isr_handler, (void*)args);
 
-
-
-
-	while(1) {
-	}
+	for(;;);
 
 	printf("Restarting in 5 seconds...\n");
 	vTaskDelay(5000 / portTICK_RATE_MS);
@@ -224,6 +225,14 @@ void app_main() {
 	pwm_stop(0);
 
 	fflush(stdout);
-    esp_restart();
+	esp_restart();
 }
 
+
+
+/* TO READ:
+ *	- https://forums.freertos.org/t/tasks-v-s-threads/16757
+ *	- https://www.freertos.org/implementation/main.html
+ *
+ *
+ */
